eefe65a21f7f7044ed412ae38ccf122fd4d8233f
unknown <shawnpark2397@gmail.com>
2023-09-13 08:10:51 -0700

    dd

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavbarComponent from './Navbar';
+
+function Header() {
+  return (
+    <header>
+      <NavbarComponent />
+    </header>
+  );
+}
+
+export default Header;
